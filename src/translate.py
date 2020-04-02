2003b79b62e67e8e3104d8d1298de173c8536b81
Derryn Edwards <derryn_93@live.com>
2020-04-02 17:21:31 +0000

    Initial commit

---
+++ src/translate.py
@@ -0,0 +1,7 @@
+import boto3
+
+client = boto3.client('translate', region_name='us-west-2')
+text = 'Hola mi nombre es Derryn'
+result = client.translate_text(Text=text, SourceLanguageCode='auto',
+    TargetLanguageCode='en')
+print(result['TranslatedText'])
